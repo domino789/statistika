0fc25c6f895840fa441e84524c923dfccabaafb9
Miloslav Staněk <445225@mail.muni.cz>
2019-04-17 13:43:08 +0200

    update cviko 1..5 and add prednasky

--- Cviko 3 - podminena prst/cv-03-01.R
+++ Cviko 3 - podminena prst/cv-03-01.R
@@ -4,6 +4,7 @@ library (prob)
 
 kostky <- rolldie (2)
 S <- probspace (kostky)
+S
 
 # A = 2 petky
 # B = soucet delitelny peti
@@ -12,4 +13,9 @@ A <- subset (S, X1 == 5 & X2 == 5)
 B <- subset (S, (X1 + X2) %% 5 == 0)
 AB <- intersect (A, B)
 Prob (AB) / Prob (B)
-choose(5,3)
+
+# Prvni odpovednik
+Z <- subset (S, (X1 == 6 | X2 == 6))
+V <- subset (S, (X1 + X2) > 10)
+ZV <- intersect (Z, V)
+Prob(ZV) / Prob(Z)
